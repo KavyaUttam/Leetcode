439bf39e98c9d8b33d0d35fa1d28951d73ccf8bb
Kavya Uttam <kavyagpatel17@gmail.com>
2026-03-19 10:44:36 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (43.55%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.java
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.java
@@ -1,35 +1,12 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int n = nums.length;
-        
-        Queue<Integer> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-        
-        queue.offer(start);
-        visited[start] = true;
-        int distance = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int curr = queue.poll();
-               if (nums[curr] == target) {
-                    return distance;
-                }
-                // Move left
-                if (curr - 1 >= 0 && !visited[curr - 1]) {
-                    queue.offer(curr - 1);
-                    visited[curr - 1] = true;
-                }
-                // Move right
-                if (curr + 1 < n && !visited[curr + 1]) {
-                    queue.offer(curr + 1);
-                    visited[curr + 1] = true;
-                }
-            }
-            
-            distance++;
+      int n=nums.length;
+      int min=Integer.MAX_VALUE; 
+      for(int i=0;i<n;i++){
+        if(nums[i]==target){
+            min= Math.min(min,Math.abs(i-start));
         }
-        
-        return -1;
+      }
+      return min;
     }
 }
