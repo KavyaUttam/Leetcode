9f879291c0e4948cda6f5ca28e8779ac7eacb9f7
Kavya Uttam <kavyagpatel17@gmail.com>
2026-04-23 10:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (76.26%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,17 @@
 class Solution {
     static int[]dp;
     public int rob(int[] nums) { 
-       dp=new int[nums.length];
+        int n=nums.length;
+       dp=new int[n+1];
        for (int i = 0; i < nums.length; i++) {
-           dp[i] = -1;
+           dp[i] = 0;
        }
-       return backtrack(nums,0);
+       dp[0]=0;
+       dp[1]=nums[0];
+       for(int i=2;i<=n;i++){
+        dp[i]=Math.max(dp[i-2] + nums[i-1], dp[i-1]);
+       }
+       return dp[n];
     }
-    private int backtrack(int[] nums, int n){
-      if(n>=nums.length)return 0;
-            if(dp[n]!=-1) return dp[n];
-      //select ith index , then i+2
-      int selecthouse=nums[n] + backtrack(nums,n+2);
-      //skip the house and move to the next
-       int skiphouse=backtrack(nums,n+1);
-            
-       //return the max of two choices
-       return dp[n]= Math.max(selecthouse,skiphouse);
-    }
+    
 }
