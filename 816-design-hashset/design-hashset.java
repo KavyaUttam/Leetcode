23ba0c40708bf0f6cc933d544e820ebc128c7f91
Kavya Uttam <kavyagpatel17@gmail.com>
2026-05-18 00:07:25 +0530

    Time: 12 ms (99.41%) | Memory: 52.2 MB (51.17%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,20 +1,66 @@
 class MyHashSet {
-    private boolean[] mp;
+    private class Node {
+        int key;
+        Node next;
+
+        Node(int key) {
+            this.key = key;
+            this.next = null;
+        }
+    }
+
+    private final Node[] set;
+    private final int SIZE = 10000;
+
+    public int getIndex(int key) {
+        return key % SIZE;
+    }
 
     public MyHashSet() {
-        mp = new boolean[1000001];
-        Arrays.fill(mp, false);
+        set = new Node[SIZE];
     }
 
     public void add(int key) {
-        mp[key] = true;
+        int index = getIndex(key);
+        Node head = set[index];
+        Node curr = head;
+        while (curr != null) {
+            if (curr.key == key) {
+                return;
+            }
+            curr = curr.next;
+        }
+        Node newNode = new Node(key);
+        newNode.next = head;
+        set[index] = newNode;
     }
 
     public void remove(int key) {
-        mp[key] = false;
+        int index = getIndex(key);
+        Node curr = set[index];
+        Node prev = null;
+        while (curr != null) {
+            if (curr.key == key) {
+                if (prev == null) {
+                    set[index] = curr.next;
+                } else {
+                    prev.next = curr.next;
+                }
+            }
+            prev = curr;
+            curr = curr.next;
+        }
     }
 
     public boolean contains(int key) {
-        return mp[key];
+        int index = getIndex(key);
+        Node curr = set[index];
+        while (curr != null) {
+            if (curr.key == key) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
     }
 }
