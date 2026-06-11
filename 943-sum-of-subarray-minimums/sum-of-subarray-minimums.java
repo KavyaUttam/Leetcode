fb7ffca7512c933c4a697361e6ed4d917256225c
Kavya Uttam <kavyagpatel17@gmail.com>
2026-06-11 15:11:11 +0530

    Time: 133 ms (5.15%) | Memory: 69.8 MB (5.83%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,56 +1,24 @@
 import java.util.Stack;
-import java.util.ArrayList;
-import java.util.Collections;
 
 class Solution {
-    static ArrayList<Integer> NSL(int[] arr) {
-        Stack<Integer> s = new Stack<>();
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                res.add(i + 1); 
-            } else {
-                res.add(i - s.peek()); 
-            }
-            s.push(i); 
-        }
-        return res;
-    }
-
-    public static ArrayList<Integer> NSR(int[] arr) {
-        Stack<Integer> s = new Stack<>();
-        ArrayList<Integer> res = new ArrayList<>();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            while (!s.isEmpty() && arr[s.peek()] > arr[i]) {
-                s.pop();
-            }
-            if (s.isEmpty()) {
-                res.add(arr.length - i);
-            } else {
-                res.add(s.peek() - i);
-            }
-            s.push(i); 
-        }
-        Collections.reverse(res); 
-        return res;
-    }
-
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         long total = 0;
         long M = (long) 1e9 + 7;
-        ArrayList<Integer> leftList = NSL(arr); 
-        ArrayList<Integer> rightList = NSR(arr);
+        Stack<Integer> s = new Stack<>();
         
-        for (int i = 0; i < n; i++) {
-            long left = leftList.get(i);
-            long right = rightList.get(i);
+        for (int i = 0; i <= n; i++) {
+            int currentVal = (i == n) ? -1 : arr[i];
             
-            long count = (left * right) % M;
-            total = (total + (count * arr[i]) % M) % M;
+            while (!s.isEmpty() && arr[s.peek()] >= currentVal) {
+                int mid = s.pop();
+                long right = i - mid; 
+                long left = s.isEmpty() ? (mid + 1) : (mid - s.peek()); 
+                
+                long count = (left * right) % M;
+                total = (total + (count * arr[mid]) % M) % M;
+            }
+            s.push(i);
         }
         
         return (int) total;
