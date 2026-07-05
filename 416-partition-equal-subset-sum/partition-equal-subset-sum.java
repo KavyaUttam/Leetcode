fcd1a37489ded5077dd65543d479201a8e16951b
Kavya Uttam <kavyagpatel17@gmail.com>
2026-07-05 22:17:22 +0530

    Time: 44 ms (58.83%) | Memory: 50 MB (39.19%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -17,14 +17,14 @@ class Solution {
     }
  private boolean subsetSum(int[] nums, int n, int sum) {
         dp = new boolean[n + 1][sum + 1];
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n+1; i++) {
             dp[i][0] = true;
         }
-        for (int j = 1; j <= sum; j++) {
+        for (int j = 1; j < sum+1; j++) {
             dp[0][j] = false; 
         }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < sum+1; j++) {
                 if (nums[i - 1] <= j) {
                     dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
                 } else {
