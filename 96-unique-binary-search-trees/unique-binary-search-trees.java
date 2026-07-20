5b121aef3d1045d6e0019a343ae09a6614aa7137
Kavya Uttam <kavyagpatel17@gmail.com>
2026-07-20 14:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (49.34%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,14 +1,25 @@
 class Solution {
     public int numTrees(int n) {
-  if(n==0 ||n==1){
-    return 1;
+  int [] dp= new int[n+1];
+  dp[0] = dp[1] = 1;
+    
+  for(int i=2; i<=n; i++) {
+    int l=0;
+    int r=i-1;
+    int res=0;
+    while(l<=r){
+        if(l==r){
+            res+=dp[l]*dp[r];
+        }
+        else{
+           res+=2*(dp[l]*dp[r]);
+        }
+    
+    l++;
+    r--;
+    }
+     dp[i] = res;
   }
-  int ans=0;
-  for(int i=1;i<=n;i++){
-    ans+=numTrees(i-1)*numTrees(n-i);
-  }
-  return ans;
+  return dp[n];
 }
 }
-
-// we can use the formula[2nCn/n+1]to implement 
