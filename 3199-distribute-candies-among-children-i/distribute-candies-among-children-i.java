3c0175c737129f3558b5450ae5e81fc43340e0b3
Kavya Uttam <kavyagpatel17@gmail.com>
2026-07-28 23:09:57 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (57.14%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
@@ -1,16 +1,20 @@
 class Solution {
     public int distributeCandies(int n, int limit) {
-      int count=0;
-      for(int first=0;first<=limit;first++){
-        for(int second=0;second<=limit;second++){
-         for(int third=0;third<=limit;third++){
-            if(first+second+third==n){
-                count++;
-            }
-         
-    }
-    } 
-      } 
-    return count;  
+        if (limit * 3 < n) {
+            return 0;
+        }
+        // how many combination of 3 numbers that sum up to n but none of them exceed limit
+        int res = 0;
+        for (int i = Math.min(limit, n); i >= 0; i--) {
+            // pick for the first num
+            int n2 = n - i;
+            if (n2 > limit * 2) continue;
+            // how many combination of 2 numbers that sum up to n2 but none of them exceed limit
+            int j = Math.min(limit, n2); // max we can pick from the second one
+            // we need to do a binary search here
+            int k = Math.max(0, n2 - j);
+            res += j - k + 1;
+        }
+        return res;
     }
 }
